a793a324944743fbc1e04c2e7eb05c7f5f489671
Tejas Srinivasan <chintu11004@gmail.com>
2026-03-04 18:50:12 -0600

    jungle-warriors is now the same as untitled

---
+++ Jungle Warrior/assets/engine/include/rope_swing.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "vm.h"
+
+BANKREF_EXTERN(ROPE_SWING)
+UBYTE rope_swing_update(void *THIS, UBYTE start, UWORD *stack_frame) OLDCALL BANKED;
---
+++ Jungle Warrior/assets/engine/include/states/platform.h
@@ -0,0 +1,27 @@
+#ifndef STATE_PLATFORM_H
+#define STATE_PLATFORM_H
+
+#include <gb/gb.h>
+
+typedef enum {
+    FALL_STATE = 0,
+    GROUND_STATE,
+    JUMP_STATE,
+    DASH_STATE,
+    LADDER_STATE,
+    WALL_STATE,
+    KNOCKBACK_STATE,
+    BLANK_STATE,
+    RUN_STATE,
+    FLOAT_STATE,
+    ROPE_STATE,
+} state_e;
+
+extern state_e plat_state;
+extern state_e plat_next_state;
+
+void platform_init(void) BANKED;
+void platform_update(void) BANKED;
+void rope_trigger_enter(UWORD anchor_x, UWORD anchor_y, UBYTE block_length, UBYTE max_angle_degrees, UBYTE swing_speed, UBYTE actor_idx) BANKED;
+
+#endif
